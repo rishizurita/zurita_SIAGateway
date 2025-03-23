4621e8c05753202e65557a65483d1de3c44cbba6
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-24 03:07:32 +0800

    Minor Changes

--- app/Http/Controllers/User2Controller.php
+++ app/Http/Controllers/User2Controller.php
@@ -19,12 +19,12 @@ class UserController extends Controller
 
     /* Helper method to return a successful JSON response */
     protected function successResponse($data, $code = Response::HTTP_OK){
-        return response()->json(['data' => $data], $code);
+
     }
 
     /* Helper method to return an error JSON response */
     protected function errorResponse($message, $code){
-        return response()->json(['error' => $message, 'code' => $code], $code);
+
     }
 
     /* Return the list of users */
