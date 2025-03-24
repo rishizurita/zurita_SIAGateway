792ee6d7298badab59b2561a2acbd17757a168d9
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-24 23:06:00 +0800

    Add CRUD methods for User2 service

--- app/Services/User2Service.php
+++ app/Services/User2Service.php
@@ -7,10 +7,6 @@ class User2Service
 {
     use ConsumesExternalService;
 
-    /**
-     * The base uri to consume the User2 Service
-     * @var string
-     */
     public $baseUri;
 
     public function __construct()
@@ -19,11 +15,52 @@ class User2Service
     }
 
     /**
-     * Obtain the full list of Users from User2 Site
-     * @return string
+     * Obtain the full list of users from Site2
+     * @return array
      */
     public function obtainUsers2()
     {
-        return $this->performRequest('GET', 'api/users'); // This code will call GET localhost:8001/users (our site2)
+        return $this->performRequest('GET', '/api/users');
+    }
+
+    /**
+     * Create a new user in Site2
+     * @param array $data
+     * @return array
+     */
+    public function createUser2($data)
+    {
+        return $this->performRequest('POST', '/api/users', $data);
+    }
+
+    /**
+     * Get a user by ID from Site2
+     * @param int $userId
+     * @return array
+     */
+    public function getUser2($userId)
+    {
+        return $this->performRequest('GET', "/api/users/{$userId}");
+    }
+
+    /**
+     * Update a user in Site2
+     * @param int $userId
+     * @param array $data
+     * @return array
+     */
+    public function updateUser2($userId, $data)
+    {
+        return $this->performRequest('PUT', "/api/users/{$userId}", $data);
+    }
+
+    /**
+     * Delete a user from Site2
+     * @param int $userId
+     * @return array
+     */
+    public function deleteUser2($userId)
+    {
+        return $this->performRequest('DELETE', "/api/users/{$userId}");
     }
 }
