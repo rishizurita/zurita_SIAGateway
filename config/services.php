2dbefdbce37c42120f7dfe3eb84106c3df61065a
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-24 22:52:27 +0800

    Updated default base URLs for User1 and User2 services

--- config/services.php
+++ config/services.php
@@ -1,9 +1,10 @@
 <?php
+
 return [
     'users1' => [
-        'base_uri' => env('USERS1_SERVICE_BASE_URL'),
+        'base_uri' => env('USER1_SERVICE_BASE_URL', 'http://localhost:8000/'),
     ],
     'users2' => [
-        'base_uri' => env('USERS2_SERVICE_BASE_URL'),
+        'base_uri' => env('USER2_SERVICE_BASE_URL', 'http://localhost:8001/'),
     ],
 ];
